8175f88f78a7bf361861017f88287f2d4d738ab7
maxjacobzander <maxjacobzander@gmail.com>
2025-05-01 18:59:50 -0400

    Updates default port to 3001 and adds app.listen

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,11 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`ReciPlanr backend listening at http://localhost:${port}`);
+});
