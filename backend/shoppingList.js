a80a508766fb98e79ac9c1197b4fccd5dd32e536
maxjacobzander <maxjacobzander@gmail.com>
2025-05-02 14:03:37 -0400

    Adds addIngredient and name standardization to shoppingList.js

--- backend/shoppingList.js
+++ backend/shoppingList.js
@@ -5,7 +5,38 @@ import { UNIT_CONVERSIONS } from "./unitConversions.js";
 // create an Object to store the shopping list --> key:value being ingredient:amount
 const shoppingList = {};
 
+// standardize ingredient name (ie egg vs eggs)
+function standardizeIngredientName(name) {
+  name = name.toLowerCase().trim();
+
+  if (name.endsWith("es") && name.length > 3) {
+    name = name.slice(0, -2);
+  } else if (name.endsWith("s") && name.length > 2) {
+    name = name.slice(0, -1);
+  }
+
+  return name;
+}
+
 // standardize measurements for the amount
+
 // find or create ingredient (string)
 // add amount to the value in the Object
+
+function addIngredient(name, amount, unit) {
+  name = standardizeIngredientName(name);
+  const { amount: convertedAmount, unit: standardUnit } =
+    standardizeMeasurement(amount, unit);
+
+  if (shoppingList[name]) {
+    shoppingList[name].quantity += convertedAmount;
+  } else {
+    shoppingList[name] = {
+      name,
+      quantity: convertedAmount,
+      unit: standardUnit,
+    };
+  }
+}
+
 // when done, return the shopping list Object
