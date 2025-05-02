cf040d87a768948b0f9d32ec6f63b4d5e4f12bab
maxjacobzander <maxjacobzander@gmail.com>
2025-05-02 00:20:06 -0400

    Add shoppingList object to shoppingList.js

--- backend/shoppingList.js
+++ backend/shoppingList.js
@@ -1,5 +1,8 @@
 // TO DO:
+
 // create an Object to store the shopping list --> key:value being ingredient:amount
+const shoppingList = {};
+
 // standardize measurements for the amount
 // find or create ingredient (string)
 // add amount to the value in the Object
