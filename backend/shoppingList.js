90406026be0b2e78dba409cee8848a9014e0b840
maxjacobzander <maxjacobzander@gmail.com>
2025-05-02 00:17:33 -0400

    Add to dos to shoppingList.js

---
+++ backend/shoppingList.js
@@ -0,0 +1,6 @@
+// TO DO:
+// create an Object to store the shopping list --> key:value being ingredient:amount
+// standardize measurements for the amount
+// find or create ingredient (string)
+// add amount to the value in the Object
+// when done, return the shopping list Object
