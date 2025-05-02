f130e9f3f077a9729b71850c98fd7d4bc52a28f5
maxjacobzander <maxjacobzander@gmail.com>
2025-05-02 00:14:53 -0400

    Add comments for some to dos

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
+// send recipes to be handled by shoppingList.js
+
+// GET request (app.get) to get the shopping_list
+
 app.listen(port, () => {
   console.log(`ReciPlanr backend listening at http://localhost:${port}`);
 });
