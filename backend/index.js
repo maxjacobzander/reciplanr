9c2d3f745a3ece4ba95c535d767e343fc796cd29
maxjacobzander <maxjacobzander@gmail.com>
2025-05-03 23:01:53 -0400

    Updates routes for new method and for text box submission

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
 import { addIngredient, getShoppingListArray } from "./shoppingList.js";
+import {
+  fetchIngredientsAndParse,
+  parseAndAddIngredients,
+} from "./fetchIngredientsAndParse.js";
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -9,11 +13,46 @@ app.use(cors());
 app.use(express.json());
 
 // send recipes to be handled by shoppingList.js
+// link input recipes
+app.post("/ingredients-from-link", async (request, response) => {
+  const url = request.body.url;
+  if (!url) return response.status(400).json({ error: "No url provided" });
+
+  const standardizedUrl = `cooked.wiki/${url}`;
+  // fetch link + parse out ingredients
+  try {
+    await fetchIngredientsAndParse(standardizedUrl);
+    response.json({ message: "Ingredients added successfully" });
+  } catch (error) {
+    console.error("Error parsing recipe:", error);
+    response.status(500).json({ error: "Failed to parse recipe" });
+  }
+});
+
+// text input receipes:
+app.post("/ingredients", (request, response) => {
+  try {
+    const text_input = request.body.text;
+
+    const ingredients = text_input
+      .split("\n")
+      .map((line) => line.trim())
+      .filter((line) => line.length > 0);
+
+    parseAndAddIngredients(ingredients);
+    response.json({ message: "Ingredients added successfully" });
+  } catch (error) {
+    console.error("Error parsing recipe:", error);
+    response.status(500).json({ error: "Failed to parse recipe" });
+  }
+});
 
 // GET request (app.get) to get the shopping_list
 app.get("/shoppinglist", (request, response) => {
   // log out to test
   console.log(`you've reached the shopping list!`);
+  const list = getShoppingListArray();
+  response.json(list);
 });
 
 app.listen(port, () => {
