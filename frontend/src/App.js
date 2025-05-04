7bc553c5330056e43a49154e6ae3aa6815eeae1b
maxjacobzander <maxjacobzander@gmail.com>
2025-05-04 00:12:16 -0400

    Removed redundant frontend folder. Added logo v1 to placeholder app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import logo_v1 from "./img/logo_v1.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo_v1} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
