5b9e32f79298bf211ee5799612639c6bffef57c7
maxjacobzander <maxjacobzander@gmail.com>
2025-05-05 16:24:29 -0400

    Add const to adapt text input, set shoppingList response, and remove AddIngrdient from index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { addIngredient, getShoppingListArray } from "./shoppingList.js";
+import { getShoppingListArray } from "./shoppingList.js";
 import {
   fetchIngredientsAndParse,
   parseAndAddIngredients,
@@ -15,14 +15,16 @@ app.use(express.json());
 // send recipes to be handled by shoppingList.js
 // link input recipes
 app.post("/ingredients-from-link", async (request, response) => {
+  console.log("hello", request.body);
   const url = request.body.url;
   if (!url) return response.status(400).json({ error: "No url provided" });
 
-  const standardizedUrl = `cooked.wiki/${url}`;
+  const standardizedUrl = `https://cooked.wiki/${url}`;
+  console.log("standardized url", standardizedUrl);
   // fetch link + parse out ingredients
   try {
     await fetchIngredientsAndParse(standardizedUrl);
-    response.json({ message: "Ingredients added successfully" });
+    response.json({ shoppingList: getShoppingListArray() });
   } catch (error) {
     console.error("Error parsing recipe:", error);
     response.status(500).json({ error: "Failed to parse recipe" });
@@ -34,13 +36,16 @@ app.post("/ingredients", (request, response) => {
   try {
     const text_input = request.body.text;
 
-    const ingredients = text_input
-      .split("\n")
+    const adapted = text_input.includes("\n")
+      ? text_input.split("\n")
+      : text_input.split(",");
+
+    const ingredients = adapted
       .map((line) => line.trim())
       .filter((line) => line.length > 0);
 
     parseAndAddIngredients(ingredients);
-    response.json({ message: "Ingredients added successfully" });
+    response.json({ shoppingList: getShoppingListArray() });
   } catch (error) {
     console.error("Error parsing recipe:", error);
     response.status(500).json({ error: "Failed to parse recipe" });
