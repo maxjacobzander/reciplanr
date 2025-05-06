45ac0e09f1df49bedf6ef0a1c6752f78581c75e0
maxjacobzander <maxjacobzander@gmail.com>
2025-05-06 19:42:25 -0400

    Updates fetchIngredientsAndParse to take normal url and imports scraper to use

--- backend/fetchIngredientsAndParse.js
+++ backend/fetchIngredientsAndParse.js
@@ -1,29 +1,12 @@
 import { JSDOM } from "jsdom";
 import { addIngredient } from "./shoppingList.js";
 import { IGNORE_WORDS } from "./ignoreWords.js";
+import { scrapeRecipe } from "./scraper.js";
 
-async function fetchIngredientsAndParse(standardizedUrl) {
-  const response = await fetch(standardizedUrl);
-  // grab the text from the page
-  const html = await response.text();
-
-  const dom = new JSDOM(html);
-  const document = dom.window.document;
-
-  // when you add cooked.wiki to the front of a link, it generates a new page, upon which there is a <div> element with a class of "shopping-list sticky" containing a list and each <li> contains an itemprop called recipeIngredient.
-
-  const ingredients = document.querySelectorAll(
-    '[itemprop="recipeIngredient"]'
-  );
-
-  // iterate through ingredients and get content
-
-  const ingredientsList = Array.from(ingredients).map((el) =>
-    // for jsdom, I needed to change innerText to textContent
-    el.textContent.trim()
-  );
-  console.log(ingredientsList);
-  parseAndAddIngredients(ingredientsList);
+async function fetchIngredientsAndParse(url) {
+  const { ingredients } = await scrapeRecipe(url);
+  console.log(ingredients);
+  parseAndAddIngredients(ingredients);
 }
 
 function cleanIngredientName(name) {
