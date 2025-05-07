5dbeb5bd39948fec4e3c585b33033baff55b777d
maxjacobzander <maxjacobzander@gmail.com>
2025-05-06 22:33:48 -0400

    Makes shoppinglist unordered list

--- frontend/src/ShoppingList.js
+++ frontend/src/ShoppingList.js
@@ -5,11 +5,11 @@ function ShoppingList({ shoppingList }) {
         <h2>Your Shopping List</h2>
         <ul>
           {shoppingList.map((item, index) => (
-            <li key={index}>
+            <ui key={index}>
               <input type="checkbox" />
               {/* React can't render objects as children, so I need render indiviudal fields here */}
               {item.quantity} {item.unit} {item.name}
-            </li>
+            </ui>
           ))}
         </ul>
       </div>
