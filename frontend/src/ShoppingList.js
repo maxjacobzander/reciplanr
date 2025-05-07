b6876d458912b7c25544b92a8ffe9cf0be0c683e
maxjacobzander <maxjacobzander@gmail.com>
2025-05-06 22:31:52 -0400

    Add conditional rendering for shopping list

--- frontend/src/ShoppingList.js
+++ frontend/src/ShoppingList.js
@@ -1,18 +1,20 @@
 function ShoppingList({ shoppingList }) {
-  return (
-    <div>
-      <h2>Your Shopping List</h2>
-      <ul>
-        {shoppingList.map((item, index) => (
-          <li key={index}>
-            <input type="checkbox" />
-            {/* React can't render objects as children, so I need render indiviudal fields here */}
-            {item.quantity} {item.unit} {item.name}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  if (shoppingList.length) {
+    return (
+      <div>
+        <h2>Your Shopping List</h2>
+        <ul>
+          {shoppingList.map((item, index) => (
+            <li key={index}>
+              <input type="checkbox" />
+              {/* React can't render objects as children, so I need render indiviudal fields here */}
+              {item.quantity} {item.unit} {item.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default ShoppingList;
