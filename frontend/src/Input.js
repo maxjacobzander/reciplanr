0cfa4239d7ebe4aa2ae1d9cf624871c93a523a33
maxjacobzander <maxjacobzander@gmail.com>
2025-05-15 12:03:52 -0400

    Change back due to a difference with Redis vs. LocalStorage

--- frontend/src/Input.js
+++ frontend/src/Input.js
@@ -34,7 +34,7 @@ function InputIngredients({ type, label, setShoppingList, placeholder }) {
 
       const data = await response.json();
       console.log("Response data:", data);
-      setShoppingList(data.shoppingList);
+      setShoppingList((prevList) => [...prevList, ...data.shoppingList]);
       setInputValue("");
       setErrorMessage("");
     } catch (error) {
